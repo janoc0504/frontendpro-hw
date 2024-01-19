405a9d7df53ca806f4b768b3b03c94b3da4a3080
janoc0504 <janoc0504@gmail.com>
2024-01-19 21:55:03 +0200

    first commit hw-04

---
+++ js04.js
@@ -0,0 +1,7 @@
+let firstNumber = +prompt("Input first number");
+let secondNumber = +prompt("Input second number");
+
+alert(secondNumber === 0 ? "You input second number 0! You can't division by 0!!!" : `${firstNumber}+${secondNumber}=${firstNumber + secondNumber}
+${firstNumber}-${secondNumber}=${firstNumber - secondNumber}
+${firstNumber}*${secondNumber}=${firstNumber * secondNumber}
+${firstNumber}/${secondNumber}=${firstNumber / secondNumber}`);
