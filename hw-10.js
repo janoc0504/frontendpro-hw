4d4ff7fda250455b63b5c45e5576f4455897fd7a
janoc0504 <janoc0504@gmail.com>
2024-01-28 20:58:15 +0200

    added up to 9

--- hw-10.js
+++ hw-10.js
@@ -33,3 +33,80 @@ let array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63,
 
 // console.log(maxEl);
 // console.log(array.indexOf(maxEl));
+
+//4.Визначити кількість негативних елементів.
+// let arrayNegEl = [];
+// array.forEach(function (item) {
+//   if (item < 0) {
+//     arrayNegEl.push(item);
+//   }
+// })
+// console.log(arrayNegEl.length);
+
+//5.Знайти кількість непарних позитивних елементів.
+// let arrayPosEl = [];
+// array.forEach(function (item) {
+//   if (item > 0) {
+//     if (item % 2 !== 0) {
+//       arrayPosEl.push(item);
+//     }
+//   }
+// })
+// console.log(arrayPosEl);
+// console.log(arrayPosEl.length);
+
+//6.Знайти кількість парних позитивних елементів.
+// let arrayPosEl = [];
+// array.forEach(function (item) {
+//   if (item > 0) {
+//     if (item % 2 === 0) {
+//       arrayPosEl.push(item);
+//     }
+//   }
+// })
+// console.log(arrayPosEl.length);
+
+//7.Знайти суму парних позитивних елементів.
+// let arrayPosEl = [];
+// array.forEach(function (item) {
+//   if (item > 0) {
+//     if (item % 2 === 0) {
+//       arrayPosEl.push(item);
+//     }
+//   }
+// })
+// let sumPos = arrayPosEl.reduce(function (accum, currentV) {
+//   return accum + currentV;
+// })
+// console.log(sumPos);
+
+//8.Знайти суму непарних позитивних елементів.
+// let arrayNegEl = [];
+// array.forEach(element => {
+//   if (element < 0) {
+//     if (element % 2 !== 0) {
+//       arrayNegEl.push(element);
+//     }
+//   }
+// });
+
+// let sumNeg = arrayNegEl.reduce(function (accum, currentV) {
+//         return accum + currentV;
+//       }, 0);
+
+// console.log(sumNeg);
+
+//9.Знайти добуток позитивних елементів.
+// let arrayPosEl = [];
+// array.forEach(element => {
+//   if (element > 0) {
+//     arrayPosEl.push(element);
+//   }
+// });
+// let multi = arrayPosEl.reduce(function (accum, currentV) {
+//   return accum * currentV;
+// }, 1);
+// console.log(arrayPosEl);
+// console.log(multi);
+
+//10.Знайти найбільший серед елементів масиву, ост альні обнулити.
