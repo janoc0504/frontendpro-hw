5c1fad870fb45e7099efaa2975c63add52a9a0ce
janoc0504 <janoc0504@gmail.com>
2024-01-28 20:21:15 +0200

    added 2nd and 3rd tasks

---
+++ hw-10.js
@@ -0,0 +1,35 @@
+//1.Дан масив [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47] Знайти суму та кількість позитивних елементів.
+let array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+
+//2.Знайти мінімальний елемент масиву та його порядковий номер.
+// let minEl = array[0];
+
+// array.forEach(function (element, index) {
+//   console.log(`element ${element}`);
+//   console.log(`index ${index}`);
+//   if (index > 0) {
+//     console.log(`minEl was ${minEl}`);
+//     if (minEl > element) {
+//       console.log(`cheking IF minEl${minEl} > element${element}, minEl = element`)
+//       minEl = element;
+//       console.log(`minEl next ${minEl}`);
+//     }
+//   }
+// })
+
+// console.log(minEl);
+// console.log(array.indexOf(minEl));
+
+//3.Знайти максимальний елемент масиву та його порядковий номер.
+// let maxEl = array[0];
+
+// array.forEach(function (element, index) {
+//   if (index > 0) {
+//     if (maxEl < element) {
+//       maxEl = element;
+//     }
+//   }
+// })
+
+// console.log(maxEl);
+// console.log(array.indexOf(maxEl));
