24cde0938df8a98f8e63a8e7f6893ccbc72fc371
janoc0504 <janoc0504@gmail.com>
2024-01-30 01:16:22 +0200

    corrected hw-09

--- hw-09.js
+++ hw-09.js
@@ -1,8 +1,8 @@
-//Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
-const array = [1, 2, 3, 4, 5, 6, 7];
-array.forEach(function (item) {
-  if (item === 5) {
+//Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array./
+
+function removeElement(array, item) {
     array.splice(array.indexOf(item), 1);
-  }
-});
+}
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
 console.log(array);
