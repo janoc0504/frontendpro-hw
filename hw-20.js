cfc39602ffb7883f5d7df1278ec20214d9310428
janoc0504 <janoc0504@gmail.com>
2024-02-15 02:30:46 +0200

    first commit hw-20

---
+++ hw-20.js
@@ -0,0 +1,14 @@
+const arrImgs = ['./image/1.jpg', './image/2.jpg', './image/3.jpg', './image/4.jpg', './image/5.jpg', './image/6.jpg', './image/7.jpg', './image/8.jpg', './image/9.jpg'];
+const imgs = document.querySelector('#imagess');
+const btn = document.querySelector('#btn');
+
+function shuffle() {
+  for (let i of arrImgs) {
+    res = Math.floor(Math.random() * arrImgs.length);
+    document.querySelector('.im').src = arrImgs[res];
+  }
+  
+  return imgs.src = arrImgs[res];
+}
+
+btn.addEventListener('click', shuffle);
