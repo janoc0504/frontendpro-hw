0d9d770b5af6e3cd8b459dc5a11b1ba08ef0302b
janoc0504 <janoc0504@gmail.com>
2024-02-19 04:07:29 -0700

    edits

--- hw-21.js
+++ hw-21.js
@@ -1,10 +1,10 @@
 const imagesArr = ['./images/1.jpg', './images/2.jpg', './images/3.jpg', './images/4.jpg'];
 const imgs = document.querySelector('.imgs');
 const leftArrow = document.querySelector('.leftArrow');
-const rihgtArrow = document.querySelector('.rihgtArrow');
+const rightArrow = document.querySelector('.rightArrow');
 let i = 0;
 
-rihgtArrow.addEventListener('click', () => {
+rightArrow.addEventListener('click', () => {
   i++;
   imgs.src = imagesArr[i];
   imgs.style.paddingLeft = '0';
@@ -12,7 +12,7 @@ rihgtArrow.addEventListener('click', () => {
 
   if (i === imagesArr.length - 1) {
     imgs.style.paddingRight = '100px';
-    rihgtArrow.style.display = 'none';
+    rightArrow.style.display = 'none';
   }
 });
 
@@ -21,7 +21,7 @@ leftArrow.addEventListener('click', () => {
   --i;
   imgs.src = imagesArr[i];
   imgs.style.paddingRight = '0';
-  rihgtArrow.style.display = 'block';
+  rightArrow.style.display = 'block';
 
   if (i === 0) {
     imgs.style.paddingLeft = '100px';
