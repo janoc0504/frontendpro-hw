47fb0f4973aa9abdca40bf6e69fcb433da1c26fa
janoc0504 <janoc0504@gmail.com>
2024-02-22 06:53:23 -0700

    add the codes

--- hw-22.js
+++ hw-22.js
@@ -1,66 +1,141 @@
-'use strict';
+// 'use strict';
 
-let counter1 = document.querySelector('.counter1');
-let counter2 = document.querySelector('.counter2');
-let counter3 = document.querySelector('.counter3');
-let counter4 = document.querySelector('.counter4');
-let counter5 = document.querySelector('.counter5');
-let smile1 = document.querySelector('.smile1');
-let smile2 = document.querySelector('.smile2');
-let smile3 = document.querySelector('.smile3');
-let smile4 = document.querySelector('.smile4');
-let smile5 = document.querySelector('.smile5');
-let resetBtn = document.querySelector('.resetBtn');
-let number1 = 0;
-let number2 = 0;
-let number3 = 0;
-let number4 = 0;
-let number5 = 0;
+// let counter1 = document.querySelector('.counter1');
+// let counter2 = document.querySelector('.counter2');
+// let counter3 = document.querySelector('.counter3');
+// let counter4 = document.querySelector('.counter4');
+// let counter5 = document.querySelector('.counter5');
+// let smile1 = document.querySelector('.smile1');
+// let smile2 = document.querySelector('.smile2');
+// let smile3 = document.querySelector('.smile3');
+// let smile4 = document.querySelector('.smile4');
+// let smile5 = document.querySelector('.smile5');
+// let resetBtn = document.querySelector('.resetBtn');
+// let number1 = 0;
+// let number2 = 0;
+// let number3 = 0;
+// let number4 = 0;
+// let number5 = 0;
 
 
-smile1.addEventListener('click', () => {
-  number1 = localStorage.getItem('counter1');
-  number1++;
-  counter1.innerHTML = number1;
-  localStorage.setItem('counter1', number1);
-});
-counter1.textContent = (localStorage.getItem('counter1'));
+// smile1.addEventListener('click', () => {
+//   number1 = localStorage.getItem('counter1');
+//   number1++;
+//   counter1.innerHTML = number1;
+//   localStorage.setItem('counter1', number1);
+// });
+// counter1.textContent = (localStorage.getItem('counter1'));
 
-smile2.addEventListener('click', () => {
-  number2 = localStorage.getItem('counter2');
-  number2++;
-  counter2.innerHTML = number2;
-  localStorage.setItem('counter2', number2);
-});
-counter2.textContent = localStorage.getItem('counter2');
+// smile2.addEventListener('click', () => {
+//   number2 = localStorage.getItem('counter2');
+//   number2++;
+//   counter2.innerHTML = number2;
+//   localStorage.setItem('counter2', number2);
+// });
+// counter2.textContent = localStorage.getItem('counter2');
 
-smile3.addEventListener('click', () => {
-  number3 = localStorage.getItem('counter3');
-  number3++;
-  counter3.innerHTML = number3;
-  localStorage.setItem('counter3', number3);
-});
-counter3.textContent = (localStorage.getItem('counter3'));
+// smile3.addEventListener('click', () => {
+//   number3 = localStorage.getItem('counter3');
+//   number3++;
+//   counter3.innerHTML = number3;
+//   localStorage.setItem('counter3', number3);
+// });
+// counter3.textContent = (localStorage.getItem('counter3'));
 
-smile4.addEventListener('click', () => {
-  number4 = localStorage.getItem('counter4');
-  number4++;
-  counter4.innerHTML = number4;
-  localStorage.setItem('counter4', number4);
-});
-counter4.textContent = localStorage.getItem('counter4');
+// smile4.addEventListener('click', () => {
+//   number4 = localStorage.getItem('counter4');
+//   number4++;
+//   counter4.innerHTML = number4;
+//   localStorage.setItem('counter4', number4);
+// });
+// counter4.textContent = localStorage.getItem('counter4');
 
-smile5.addEventListener('click', () => {
-  number5 = localStorage.getItem('counter5');
-  number5++;
-  counter5.innerHTML = number5;
-  localStorage.setItem('counter5', number5);
-});
-counter5.textContent = localStorage.getItem('counter5');
+// smile5.addEventListener('click', () => {
+//   number5 = localStorage.getItem('counter5');
+//   number5++;
+//   counter5.innerHTML = number5;
+//   localStorage.setItem('counter5', number5);
+// });
+// counter5.textContent = localStorage.getItem('counter5');
 
-resetBtn.addEventListener('click', () => {
-  localStorage.clear();
-  for (let smile of document.querySelectorAll('.counters')) {
-    smile.textContent = '';
-  }
-});
+// resetBtn.addEventListener('click', () => {
+//   localStorage.clear();
+//   for (let smile of document.querySelectorAll('.counters')) {
+//     smile.textContent = '';
+//   }
+// });
+
+// 'use strict';
+// function createCounter(containerSelector, smileSelector, localStorageKey) {
+//   let container = document.querySelector(containerSelector);
+//   let smile = document.querySelector(smileSelector);
+//   let number = parseInt(localStorage.getItem(localStorageKey)) || 0;
+
+//   function increment() {
+//     number++;
+//     localStorage.setItem(localStorageKey, number);
+//     updateDisplay();
+//   }
+
+//   function updateDisplay() {
+//     container.textContent = number;
+//   }
+
+//   function initialize() {
+//     updateDisplay();
+//     smile.addEventListener('click', increment);
+//   }
+
+//   return { initialize };
+// }
+
+// const counters = [
+//   createCounter('.counter1', '.smile1', 'counter1'),
+//   createCounter('.counter2', '.smile2', 'counter2'),
+//   createCounter('.counter3', '.smile3', 'counter3'),
+//   createCounter('.counter4', '.smile4', 'counter4'),
+//   createCounter('.counter5', '.smile5', 'counter5')
+// ];
+
+// //Функция создания счетчика с автоматическим сбросом
+// function createCounter(containerSelector, smileSelector, localStorageKey, resetThreshold) {
+//   let container = document.querySelector(containerSelector);
+//   let smile = document.querySelector(smileSelector);
+//   let number = parseInt(localStorage.getItem(localStorageKey)) || 0;
+
+//   function increment() {
+//     number++;
+//     localStorage.setItem(localStorageKey, number);
+//     updateDisplay();
+//     // Проверяем, достигло ли значение счетчика порога для сброса
+//     if (number >= resetThreshold) {
+//       reset();
+//     }
+//   }
+
+//   function updateDisplay() {
+//     container.textContent = number;
+//   }
+
+//   function reset() {
+//     number = 0;
+//     localStorage.setItem(localStorageKey, number);
+//     updateDisplay();
+//   }
+
+//   function initialize() {
+//     updateDisplay();
+//     smile.addEventListener('click', increment);
+//   }
+
+//   return { initialize };
+// }
+
+// // Создание счетчиков с разными порогами для сброса
+// const counters = [
+//   createCounter('.counter1', '.smile1', 'counter1', 10),   // Сброс при достижении 10
+//   createCounter('.counter2', '.smile2', 'counter2', 20),   // Сброс при достижении 20
+//   createCounter('.counter3', '.smile3', 'counter3', 30),   // Сброс при достижении 30
+//   createCounter('.counter4', '.smile4', 'counter4', 40),   // Сброс при достижении 40
+//   createCounter('.counter5', '.smile5', 'counter5', 50)    // Сброс при достижении 50
+// ];
