7e1e039ee2474cb024df34e6334ce47292fcbcf1
janoc0504 <janoc0504@gmail.com>
2024-02-23 23:40:49 +0200

    new age of this code

--- hw-22.js
+++ hw-22.js
@@ -1,106 +1,51 @@
-// This id the bad code, but it works.
-
-// 'use strict';
-
-// let counter1 = document.querySelector('.counter1');
-// let counter2 = document.querySelector('.counter2');
-// let counter3 = document.querySelector('.counter3');
-// let counter4 = document.querySelector('.counter4');
-// let counter5 = document.querySelector('.counter5');
-// let smile1 = document.querySelector('.smile1');
-// let smile2 = document.querySelector('.smile2');
-// let smile3 = document.querySelector('.smile3');
-// let smile4 = document.querySelector('.smile4');
-// let smile5 = document.querySelector('.smile5');
-// let resetBtn = document.querySelector('.resetBtn');
-// let number1 = 0;
-// let number2 = 0;
-// let number3 = 0;
-// let number4 = 0;
-// let number5 = 0;
-
-
-// smile1.addEventListener('click', () => {
-//   number1 = localStorage.getItem('counter1');
-//   number1++;
-//   counter1.innerHTML = number1;
-//   localStorage.setItem('counter1', number1);
-// });
-// counter1.textContent = (localStorage.getItem('counter1'));
-
-// smile2.addEventListener('click', () => {
-//   number2 = localStorage.getItem('counter2');
-//   number2++;
-//   counter2.innerHTML = number2;
-//   localStorage.setItem('counter2', number2);
-// });
-// counter2.textContent = localStorage.getItem('counter2');
-
-// smile3.addEventListener('click', () => {
-//   number3 = localStorage.getItem('counter3');
-//   number3++;
-//   counter3.innerHTML = number3;
-//   localStorage.setItem('counter3', number3);
-// });
-// counter3.textContent = (localStorage.getItem('counter3'));
-
-// smile4.addEventListener('click', () => {
-//   number4 = localStorage.getItem('counter4');
-//   number4++;
-//   counter4.innerHTML = number4;
-//   localStorage.setItem('counter4', number4);
-// });
-// counter4.textContent = localStorage.getItem('counter4');
-
-// smile5.addEventListener('click', () => {
-//   number5 = localStorage.getItem('counter5');
-//   number5++;
-//   counter5.innerHTML = number5;
-//   localStorage.setItem('counter5', number5);
-// });
-// counter5.textContent = localStorage.getItem('counter5');
-
-// resetBtn.addEventListener('click', () => {
-//   localStorage.clear();
-//   for (let smile of document.querySelectorAll('.counters')) {
-//     smile.textContent = '';
-//   }
-// });
-
-//This's the good code and it works too.
-
 'use strict';
 
-function createCounter(containerSelector, smileSelector, localStorageKey) {
-  let container = document.querySelector(containerSelector);
-  let smile = document.querySelector(smileSelector);
+// Create a smiles arrey and a container (wrapper) for div elements with smiles
+const smilesArr = ['&#127936;', '&#9918;', '&#9917;', '&#127944;', '&#127952;', '&#129358;'];
+const wrapper = document.querySelector('.wrapper');
 
-  function increment() {
-    let number = localStorage.getItem(localStorageKey);
-    number++;
-    container.innerHTML = number;
-    localStorage.setItem(localStorageKey, number);
-  }
+// Проходимся по массиву смайлов
+//Get smiles from the smiles arrey
+smilesArr.forEach(smile => {
+  // Создаем контейнеры для смайлов и счетчиков
+  // Create containers for smiles and counters
+  const smileElement = document.createElement('div');
+  smileElement.innerHTML = smile;
+  smileElement.className = 'smiles';
 
-    container.textContent = localStorage.getItem(localStorageKey);
-    smile.addEventListener('click', increment);
+  const counterElement = document.createElement('p');
+  counterElement.textContent = getStoredCount(smile) || 0;
 
-  return smile;
-}
+  smileElement.appendChild(counterElement);
+  wrapper.appendChild(smileElement);
 
-const counters = [
-  createCounter('.counter1', '.smile1', 'counter1'),
-  createCounter('.counter2', '.smile2', 'counter2'),
-  createCounter('.counter3', '.smile3', 'counter3'),
-  createCounter('.counter4', '.smile4', 'counter4'),
-  createCounter('.counter5', '.smile5', 'counter5'),
-  createCounter('.counter6', '.smile6', 'counter6'),
-];
+  // Навешиваем кликер на смайлы и создаем счетик + записываем в LocalStorage текущее значение счетчика
+  // Crate a click function for smiles and counter and write counter current values to the localStorage
+  smileElement.addEventListener('click', () => {
+        let count = parseInt(counterElement.textContent);
+        count++;
+        counterElement.textContent = count;
 
-const resetBtn = document.querySelector('.resetBtn');
-resetBtn.addEventListener('click', () => {
-  localStorage.clear();
-    for (let smile of document.querySelectorAll('.reset')) {
-    smile.textContent = ' ';
+    localStorage.setItem(smile, count);
+  });
+});
+
+// Создаем функцию для получения количества кликов из LocalStorage
+// Create a function for geting counter values from the localStorage
+    function getStoredCount(smile) {
+      return localStorage.getItem(smile);
+    }
+
+// Навешиваем кликер нажатия на кнопку сброса
+// Create a click function for a reset button
+    document.querySelector('.resetBtn').addEventListener('click', () => {
+      // Очищаем LocalStorage
+      // Resetting the localStorage on the page
+      localStorage.clear();
+
+      // Обнуляем счетчики
+      // Resetting the counters
+     for (let smile of document.querySelectorAll('p')) {
+    smile.textContent = '0';
   }
 });
