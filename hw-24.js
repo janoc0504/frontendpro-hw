6efd6295621c51a771eec8fc434a26d54b5a8ec9
janoc0504 <janoc0504@gmail.com>
2024-03-02 02:20:27 +0200

    added returns

--- hw-24.js
+++ hw-24.js
@@ -1,21 +1,17 @@
-'use strict';
-
 let ladder = {
-  step: 0,
-  up: function () {
-    this.step++;
-  },
-  down: function () {
-    this.step--;
-  },
-  showStep: function () { // показывает текущую ступеньку
-    alert(this.step);
-  }
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        alert(this.step);
+        return this;
+    }
 };
 
-// ladder.up();
-// ladder.up();
-// ladder.down();
-// ladder.showStep(); // 1
-
-ladder.up().up().down().showStep(); // 1
+ladder.up().up().down().up().up().up().up().down().showStep();
