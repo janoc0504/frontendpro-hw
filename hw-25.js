cf59f7ca548670f62c04db61c8f565f2ef2db6e0
janoc0504 <janoc0504@gmail.com>
2024-03-06 19:06:03 -0700

    second commit+

--- hw-25.js
+++ hw-25.js
@@ -1,5 +1,5 @@
 let goodsCategory = document.querySelector('#goodsCategory');
-let listGoods = [{name: 'phone', price: '$100', }, {name: 'phone2', price: '$102'}, {name: 'phone3', price: '$103'}];
+let listGoods = [{name: 'phone', price: '$100'}, {name: 'phone2', price: '$102'}, {name: 'phone3', price: '$103'}];
 
 let elementCategory = document.createElement('div');
 elementCategory.className = 'category';
@@ -9,7 +9,13 @@ goodsCategory.appendChild(elementCategory);
 let goodsList = document.createElement('div');
 goodsCategory.appendChild(goodsList);
 
+let goodsInfo = document.createElement('div');
 
+
+function showInfo(good) {
+    goodsInfo.innerHTML = `<p>Name: ${good.name}\nPrice: ${good.price}</p>`;
+    elementCategory.appendChild(goodsInfo);
+}
 
 function oneClick() {
     listGoods.forEach(good => {
@@ -17,10 +23,13 @@ function oneClick() {
         listItem.innerHTML = `<p>Name: ${good.name}</p><p>Price: ${good.price}</p>`;
         goodsList.appendChild(listItem);
         listItem.className = 'list';
+
+        listItem.addEventListener('click', ()=> {
+            showInfo(good);
+        })
     })
 
     elementCategory.removeEventListener('click', oneClick);
 }
 
 elementCategory.addEventListener('click', oneClick);
-
