fd448b8b556f789fe3e423b237445f46c9ad6a90
janoc0504 <janoc0504@gmail.com>
2024-03-21 20:34:17 -0600

    next commit hw-29

--- hw-29.js
+++ hw-29.js
@@ -1,19 +1,57 @@
-// let age = prompt('How old are you?');
-let city = prompt('What is your current city?');
 // let sport = prompt('What kind of sport do you love?');
+// let city = prompt('What is your current city?');
 
-const currentLocation = {
-    Kyiv: 'Ukriane',
-    Vashington: 'USA',
-    London: 'The UK'
+
+function getAge(yearBirth) {
+    const currentYear = new Date().getFullYear();
+    let age = currentYear - yearBirth;
+    return age;
 }
 
-function showUserInfo() {
-    for(let item in currentLocation) {
-        if(item.trim().toLowerCase() === city.trim().toLowerCase()) {
-           return console.log(`If ${city} is the capital of your county, you are living in ${currentLocation[item]}`);
+function currentInformation() {
+    let yearBirth = +prompt('What is your year of birth?');
+    let sport = prompt('What kind of sport do you love?');
+    let city = prompt('What is your current city?');
+
+    const age = getAge(yearBirth);
+    const currentLocation = document.querySelector('#location');
+    const currentSports = document.querySelector('#sports');
+    
+    if(yearBirth === null || city === null || sport === null) {
+        currentLocation.innerHTML = `<p>BADLY BAD</p>`;
+        return
+    }
+console.log(yearBirth);
+    currentLocation.innerHTML = '';
+    if(!isNaN(age) && yearBirth !== 0) {
+        switch (city.toLowerCase()) {
+            case 'Kiev':
+                currentLocation.innerHTML = `<p>You are ${age} years old.</p><p>You are living in the capital of Ukraine.</p>`;
+                break;
+            case 'London':
+                currentLocation.innerHTML = `<p>You are ${age} years old.</p><p>You are living in the capital of Great Britain.</p>`;
+                break;
+            case 'Washington':
+                currentLocation.innerHTML = `<p>You are ${age} years old.</p><p>You are living in the capital of USA.</p>`;
+                break;
+            default:
+                currentLocation.innerHTML = `<p>You are ${age} years old.</p><p>You are living in ${city}.</p>`;
         }
+    } else {
+        currentLocation.innerHTML = `<p>Uuups!</p>`;
+    }
+    currentSports.innerHTML = '';
+    switch (sport.toLowerCase()) {
+        case 'Football':
+            currentSports.innerHTML = `<p>Do you want to be as Pele?</p>`;
+            break;
+        case 'Boxing':
+            currentSports.innerHTML = `<p>Do you want to be as Klichko?</p>`;
+            break;
+        case 'GYM':
+            currentSports.innerHTML = `<p>Do you want to be as Shvarzenegger?</p>`;
+            break;
     }
 }
 
-showUserInfo();
+currentInformation();
---
+++ new.js
@@ -0,0 +1,49 @@
+// let sport = prompt('What kind of sport do you love?');
+// let city = prompt('What is your current city?');
+// let yearBirth = +prompt('What is your year of birth?');
+
+function getAge(yearBirth) {
+    const currentYear = new Date().getFullYear();
+    let age = currentYear - yearBirth;
+    return age;
+}
+function info() {
+    let sport = prompt('What kind of sport do you love?');
+    let city = prompt('What is your current city?');
+    let yearBirth = +prompt('What is your year of birth?');
+
+    const currentLocation = document.querySelector('#location');
+    const currentSports = document.querySelector('#sports');
+    const age = getAge(yearBirth);
+
+    if(yearBirth === null || city === null || sport === null) {
+        currentLocation.innerHTML = `<p>BADLY BAD</p>`;
+        return
+    }
+
+    const sportsChampions = {
+        "football": "Messi",
+        "gym": "Shvarzenegger",
+        "box": "Klichko"
+    };
+
+    if (sportsChampions.hasOwnProperty(sport.toLowerCase())) {
+        currentSports.innerHTML = `<p>Круто! Хочеш стати ${sportsChampions[sport.toLowerCase()]}?</p>`;
+    }
+
+    function getCountry(city) {
+        const capitals = {
+            "Kiev": "України",
+            "Washington": "США",
+            "London": "Великої Британії"
+        };
+        return capitals[city.toLowerCase()];
+    }
+
+    if (city.toLowerCase() === "Kiev" || city.toLowerCase() === "Washington" || city.toLowerCase() === "London") {
+        currentLocation.innerHTML = `<p>You are ${age} years old. Ти живеш у столиці ${getCountry(city)}, ${city}.</p>`;
+    } else {
+        currentLocation.innerHTML = `<p>You are ${age} years old. Ти живеш у місті ${city}.</p>`;
+    }
+}
+info();
